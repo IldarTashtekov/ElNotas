4167ad3707d1f420dcc421e530c08bfac87ce6f3
magotago <you@example.com>
2026-01-03 13:01:04 +0100

    refactor: Add explicit typing to main.ts

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,17 +1,17 @@
 // DOM references
-const addNoteBtn = document.getElementById("add-note-btn") as HTMLButtonElement;
-const notesList = document.getElementById("notes-list") as HTMLUListElement;
+const addNoteBtn: HTMLButtonElement = document.getElementById("add-note-btn") as HTMLButtonElement;
+const notesList: HTMLUListElement = document.getElementById("notes-list") as HTMLUListElement;
 
   
 addNoteBtn.addEventListener("click",() =>{
 
   // Ask new name to the user
-  const noteText = prompt("Escribe tu nota:");
+  const noteText : string | null = prompt("Escribe tu nota:");
 
   // If the user write the new element name
   if(noteText && noteText.trim() != ""){
 
-    const li = document.createElement("li");
+    const li: HTMLLIElement = document.createElement("li");
     li.textContent = noteText
 
     // Add element to the list
